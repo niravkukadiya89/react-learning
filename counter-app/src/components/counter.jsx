57bcd66a55e314a6c76762929197096ac079a021
Nirav Kukadiya <nirav.kukadiya89@gmail.com>
2019-03-25 08:45:42 +0530

    Learning State props etc

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = { 
+    value: this.props.counter.value,
+    tags:['tag1','tag2','tag3','tag4']
+   };
+  styles = {
+    fontSize: 25,
+    fontWeight: "bold"
+  };
+
+  // constructor(){
+  //   super();
+  //   this.countIncrement = this.countIncrement.bind(this);
+  // }
+
+  countIncrement = (product) => {
+    //console.log('increment',this);
+    //console.log(product);
+    this.setState({value : this.state.value + 1});
+  }
+
+  argCountIncrement = () => {
+    this.countIncrement({id:1})
+  }
+
+  
+
+  render() {
+  //  console.log(this.props);
+    return (
+      <div>
+        {/* <h2>Header #{this.props.id}</h2> */}
+       
+       
+
+
+        <span style={this.styles} className={this.getClasses()}>{this.formatCount()}</span>
+        <button onClick={() => {this.countIncrement(1)}} className="btn btn-seconday btn-sm m-2" id="submit">
+          Increment
+        </button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+        {/* <ul>
+          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+        </ul> */}
+      </div>
+    );
+  }
+
+  getClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    //const { count } = this.state;
+    // return this.state.count === 0 ? "Zero" : this.state.count;
+    return this.state.value === 0 ? "Zero" : this.state.value;
+  }
+}
+
+export default Counter;
